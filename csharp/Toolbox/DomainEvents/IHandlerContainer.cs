c26220042299ae0263c3cbb531e2f078189016cd
Michael Benford <michael@michaelbenford.net>
2013-04-07 02:04:13 -0300

    Implemented DomainEvents class with basic event handling

---
+++ csharp/Toolbox/DomainEvents/IHandlerContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Toolbox.DomainEvents
+{
+    public interface IHandlerContainer
+    {
+        IEnumerable<IHandlerOf<T>> GetHandlersOf<T>() where T: IDomainEvent;
+    }
+}
