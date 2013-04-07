b83950966b81548fc62c66205c7283a1547fa749
Michael Benford <michael@michaelbenford.net>
2013-04-07 02:05:57 -0300

    Implemented first version of TestingContainer

---
+++ csharp/Toolbox/DomainEvents/TestContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toolbox.DomainEvents
+{
+    public class TestContainer : IHandlerContainer
+    {
+        private IDictionary<Type, object> handlers;
+
+        public TestContainer()
+        {
+            handlers = new Dictionary<Type, object>();
+        }
+
+        public IEnumerable<IHandlerOf<T>> GetHandlersOf<T>() where T : IDomainEvent
+        {
+            var handler =  new Handler<T>();
+            handlers.Add(typeof(T), handler);
+
+            return new[] { handler };
+        }
+
+        public bool WasRaised<T>() where T: IDomainEvent
+        {
+            return handlers.ContainsKey(typeof (T)) && ((Handler<T>) handlers[typeof (T)]).Handled;
+        }
+
+        class Handler<T> : IHandlerOf<T> where T : IDomainEvent
+        {
+            public void Handle(T domainEvent)
+            {
+                Handled = true;
+            }
+
+            public bool Handled { get; private set; }
+        }
+    }
+}
