427cf35b70f53fe50fed2fcd172fb0f32e70ab26
Michael Benford <michael@michaelbenford.net>
2013-04-18 02:55:38 -0300

    Implemented DefaultContainer class that loads all handlers of an event found in an assembly

---
+++ csharp/Toolbox/DomainEvents.Tests/DefaultContainerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Toolbox.DomainEvents.Test
+{
+    public class DefaultContainerTest
+    {
+        public class Loads_All_Handlers_Of_An_Event_From_The_Provided_Assembly
+        {
+            private IEnumerable<IHandlerOf<DummyEvent>> result;
+
+            [TestFixtureSetUp]
+            public void TestFixtureSetUp()
+            {
+                // Arrange
+                var sut = new DefaultContainer(Assembly.GetExecutingAssembly());
+
+                // Act
+                result = sut.GetHandlersFor<DummyEvent>();
+            }
+
+            [Test]
+            public void Result_Should_Not_Be_Null()
+            {
+                Assert.NotNull(result);
+            }
+
+            [Test]
+            public void Result_Count_Should_Be_3()
+            {
+                Assert.AreEqual(3, result.Count());
+            }
+
+            [Test]
+            public void Result_Should_Contain_DummyHandler1()
+            {
+                Assert.True(result.Any(a => a.GetType() == typeof(DummyHandler1)));
+            }
+
+            [Test]
+            public void Result_Should_Contain_DummyHandler2()
+            {
+                Assert.True(result.Any(a => a.GetType() == typeof(DummyHandler2)));
+            }
+
+            [Test]
+            public void Result_Should_Contain_DummyHandler3()
+            {
+                Assert.True(result.Any(a => a.GetType() == typeof(DummyHandler3)));
+            }
+
+        }
+    }
+}
---
+++ csharp/Toolbox/DomainEvents/DefaultContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Toolbox.DomainEvents
+{
+    public class DefaultContainer : IHandlerContainer
+    {
+        private readonly Assembly assembly;
+
+        public DefaultContainer(Assembly assembly)
+        {
+            this.assembly = assembly;
+        }
+
+        public IEnumerable<IHandlerOf<T>> GetHandlersFor<T>() where T : IDomainEvent
+        {         
+            return from type in assembly.GetTypes()
+                   where typeof(IHandlerOf<T>).IsAssignableFrom(type)
+                   select Activator.CreateInstance(type) as IHandlerOf<T>;
+        }
+    }    
+}
