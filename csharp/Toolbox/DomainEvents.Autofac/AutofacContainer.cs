10654f5d8302c3be7c3a36981ef637f0ba30d117
Michael Benford <michael@michaelbenford.net>
2013-04-26 02:02:55 -0300

    Added Autofac support

---
+++ csharp/Toolbox/DomainEvents.Autofac/AutofacContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Autofac;
+
+namespace Toolbox.DomainEvents.Autofac
+{
+    public class AutofacContainer : IHandlerContainer
+    {
+        private readonly IContainer container;
+
+        public AutofacContainer(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public IEnumerable<IHandlerOf<T>> GetHandlersOf<T>() where T : IDomainEvent
+        {            
+            return container.Resolve<IEnumerable<IHandlerOf<T>>>();
+        }
+    }
+}
