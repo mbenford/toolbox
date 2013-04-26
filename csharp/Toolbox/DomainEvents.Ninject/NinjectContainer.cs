0c3d3c459a3150508b322ca4b19a502707893555
Michael Benford <michael@michaelbenford.net>
2013-04-26 01:34:26 -0300

    Added Ninject support

---
+++ csharp/Toolbox/DomainEvents.Ninject/NinjectContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Ninject;
+
+namespace Toolbox.DomainEvents.Ninject
+{
+    public class NinjectContainer : IHandlerContainer
+    {
+        private readonly IKernel kernel;
+
+        public NinjectContainer(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public IEnumerable<IHandlerOf<T>> GetHandlersOf<T>() where T : IDomainEvent
+        {
+            return kernel.GetAll<IHandlerOf<T>>();
+        }
+    }
+}
